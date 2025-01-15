9fbf71cde4a32c08f70b530475382773c0742ba0
Luis Armando Alvarez Zapfe <lalvarezzapfe@gmail.com>
2025-01-14 19:51:31 -0600

    vercel connect

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default {
-  css: {
-    devSourcemap: false,
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "dist", // Asegúrate de que el directorio de salida sea correcto
+    rollupOptions: {
+      input: "index.html", // Punto de entrada
+    },
   },
-};
+  server: {
+    port: 5173,
+    open: true,
+  },
+});
