a31d8a56418ad001de8491083a6a7f49579b002a
Luis Armando Alvarez Zapfe <lalvarezzapfe@gmail.com>
2025-01-14 19:54:10 -0600

    vercel 3

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist", // Asegúrate de que el directorio de salida sea correcto
-    rollupOptions: {
-      input: "index.html", // Punto de entrada
-    },
+    outDir: "dist",
   },
   server: {
     port: 5173,
-    open: true,
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
   },
 });
